992dae9260903d617b4ac93e6b63262ca8ac7098
Suneet Misra <hello@suneet.codes>
2024-12-20 14:19:56 +0000

    💄 Updates Paragraph tag
    Updates Paragraph component to use React.memo to improve performance when the component is rendered within a list. This is in line with PR feedback from coderabbit

--- components/ui/paragraph.tsx
+++ components/ui/paragraph.tsx
@@ -1,7 +1,10 @@
+import { memo } from "react";
+
 import { cn } from "@/lib/utils";
 
 type ParagraphProps = React.ComponentPropsWithoutRef<"p">;
-export const P = ({ children, className, ...rest }: ParagraphProps) => {
+
+export const P = memo(({ children, className, ...rest }: ParagraphProps) => {
   return (
     <p
       className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
@@ -10,4 +13,6 @@ export const P = ({ children, className, ...rest }: ParagraphProps) => {
       {children}
     </p>
   );
-};
+});
+
+P.displayName = "P";
