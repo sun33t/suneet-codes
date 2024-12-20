427d8746d87793dbbe598e5b25d6e309732e2a6b
Suneet Misra <hello@suneet.codes>
2024-12-20 23:09:58 +0000

    💄 Adds Header component
    Header component added with mobile and desktop navigation. Aslo fixed styles for mode toggle and added Avatar component.

---
+++ components/header.tsx
@@ -0,0 +1,389 @@
+"use client";
+
+import { Container } from "./container";
+import { ModeToggle } from "./mode-toggle";
+
+import clsx from "clsx";
+import { ChevronDown } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import avatarImage from "@/images/avatar.jpeg";
+
+const pages: Array<{ title: string; slug: string }> = [
+  { title: "About", slug: "/about" },
+  { title: "Articles", slug: "/articles" },
+  { title: "Projects", slug: "/projects" },
+  { title: "Speaking", slug: "/speaking" },
+  { title: "Uses", slug: "/uses" },
+];
+
+function clamp(number: number, a: number, b: number) {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  return Math.min(Math.max(number, min), max);
+}
+
+const AvatarContainer = ({
+  className,
+  ...rest
+}: React.ComponentPropsWithoutRef<"div">) => {
+  return (
+    <div
+      className={clsx(
+        className,
+        "h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
+      )}
+      {...rest}
+    />
+  );
+};
+
+const Avatar = ({
+  large = false,
+  className,
+  ...rest
+}: Omit<React.ComponentPropsWithoutRef<typeof Link>, "href"> & {
+  large?: boolean;
+}) => {
+  return (
+    <Link
+      href="/"
+      aria-label="Home"
+      className={clsx(className, "pointer-events-auto")}
+      {...rest}
+    >
+      <Image
+        src={avatarImage}
+        alt=""
+        sizes={large ? "4rem" : "2.25rem"}
+        className={clsx(
+          "rounded-full bg-zinc-100 object-cover dark:bg-zinc-800",
+          large ? "h-16 w-16" : "h-9 w-9"
+        )}
+        priority
+      />
+    </Link>
+  );
+};
+
+function NavItem({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const isActive = usePathname() === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={clsx(
+          "relative block px-3 py-2 transition",
+          isActive
+            ? "text-teal-500 dark:text-teal-400"
+            : "hover:text-teal-500 dark:hover:text-teal-400"
+        )}
+      >
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+        )}
+      </Link>
+    </li>
+  );
+}
+
+function DesktopNavigation(props: React.ComponentPropsWithoutRef<"nav">) {
+  return (
+    <nav {...props}>
+      <ul className="flex rounded-md bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        {pages.map((page, index) => (
+          <NavItem key={`${index}-${page.slug}-desktop`} href={page.slug}>
+            {page.title}
+          </NavItem>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+const MobileNavItem = ({
+  page,
+  setIsMenuOpen,
+}: {
+  page: { title: string; slug: string };
+  setIsMenuOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const isActive = usePathname() === page.slug;
+  return (
+    <DropdownMenuItem
+      className={clsx(
+        "text-sm font-medium",
+        isActive
+          ? "text-teal-500 dark:text-teal-400"
+          : "text-zinc-800 dark:text-zinc-200"
+      )}
+      onClick={() => setIsMenuOpen(false)}
+    >
+      <Link href={page.slug} legacyBehavior passHref>
+        {page.title}
+      </Link>
+    </DropdownMenuItem>
+  );
+};
+
+const MobileNavigation = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  return (
+    <DropdownMenu open={isMenuOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="pointer-events-auto rounded-md bg-white/90 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur aria-expanded:text-teal-500 md:hidden dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 aria-expanded:dark:text-teal-400"
+          variant="outline"
+        >
+          Menu
+          <ChevronDown />
+          <span className="sr-only">Toggle Mobile Menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="rounded-md bg-white/90 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+        onInteractOutside={() => setIsMenuOpen(false)}
+        onEscapeKeyDown={() => setIsMenuOpen(false)}
+      >
+        {pages.map((page, index) => {
+          return (
+            <MobileNavItem
+              key={`${index}-${page.title}`}
+              page={page}
+              setIsMenuOpen={setIsMenuOpen}
+            />
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export const Header = () => {
+  const isHomePage = usePathname() === "/";
+
+  const headerRef = useRef<React.ComponentRef<"div">>(null);
+  const avatarRef = useRef<React.ComponentRef<"div">>(null);
+  const isInitial = useRef(true);
+
+  useEffect(() => {
+    const downDelay = avatarRef.current?.offsetTop ?? 0;
+    const upDelay = 64;
+
+    function setProperty(property: string, value: string) {
+      document.documentElement.style.setProperty(property, value);
+    }
+
+    function removeProperty(property: string) {
+      document.documentElement.style.removeProperty(property);
+    }
+
+    function updateHeaderStyles() {
+      if (!headerRef.current) {
+        return;
+      }
+
+      const { top, height } = headerRef.current.getBoundingClientRect();
+      const scrollY = clamp(
+        window.scrollY,
+        0,
+        document.body.scrollHeight - window.innerHeight
+      );
+
+      if (isInitial.current) {
+        setProperty("--header-position", "sticky");
+      }
+
+      setProperty("--content-offset", `${downDelay}px`);
+
+      if (isInitial.current || scrollY < downDelay) {
+        setProperty("--header-height", `${downDelay + height}px`);
+        setProperty("--header-mb", `${-downDelay}px`);
+      } else if (top + height < -upDelay) {
+        const offset = Math.max(height, scrollY - upDelay);
+        setProperty("--header-height", `${offset}px`);
+        setProperty("--header-mb", `${height - offset}px`);
+      } else if (top === 0) {
+        setProperty("--header-height", `${scrollY + height}px`);
+        setProperty("--header-mb", `${-scrollY}px`);
+      }
+
+      if (top === 0 && scrollY > 0 && scrollY >= downDelay) {
+        setProperty("--header-inner-position", "fixed");
+        removeProperty("--header-top");
+        removeProperty("--avatar-top");
+      } else {
+        removeProperty("--header-inner-position");
+        setProperty("--header-top", "0px");
+        setProperty("--avatar-top", "0px");
+      }
+    }
+
+    function updateAvatarStyles() {
+      if (!isHomePage) {
+        return;
+      }
+
+      const fromScale = 1;
+      const toScale = 36 / 64;
+      const fromX = 0;
+      const toX = 2 / 16;
+
+      const scrollY = downDelay - window.scrollY;
+
+      let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale;
+      scale = clamp(scale, fromScale, toScale);
+
+      let x = (scrollY * (fromX - toX)) / downDelay + toX;
+      x = clamp(x, fromX, toX);
+
+      setProperty(
+        "--avatar-image-transform",
+        `translate3d(${x}rem, 0, 0) scale(${scale})`
+      );
+
+      const borderScale = 1 / (toScale / scale);
+      const borderX = (-toX + x) * borderScale;
+      const borderTransform = `translate3d(${borderX}rem, 0, 0) scale(${borderScale})`;
+
+      setProperty("--avatar-border-transform", borderTransform);
+      setProperty("--avatar-border-opacity", scale === toScale ? "1" : "0");
+    }
+
+    function updateStyles() {
+      updateHeaderStyles();
+      updateAvatarStyles();
+      isInitial.current = false;
+    }
+
+    updateStyles();
+    window.addEventListener("scroll", updateStyles, { passive: true });
+    window.addEventListener("resize", updateStyles);
+
+    return () => {
+      window.removeEventListener("scroll", updateStyles);
+      window.removeEventListener("resize", updateStyles);
+    };
+  }, [isHomePage]);
+
+  return (
+    <>
+      <header
+        className="pointer-events-none relative z-50 flex flex-none flex-col"
+        style={{
+          height: "var(--header-height)",
+          marginBottom: "var(--header-mb)",
+        }}
+      >
+        {isHomePage && (
+          <>
+            <div
+              ref={avatarRef}
+              className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
+            />
+            <Container
+              className="top-0 order-last -mb-3 pt-3"
+              style={{
+                position:
+                  "var(--header-position)" as React.CSSProperties["position"],
+              }}
+            >
+              <div
+                className="top-[var(--avatar-top,theme(spacing.3))] w-full"
+                style={{
+                  position:
+                    "var(--header-inner-position)" as React.CSSProperties["position"],
+                }}
+              >
+                <div className="relative">
+                  <AvatarContainer
+                    className="absolute left-0 top-3 origin-left transition-opacity"
+                    style={{
+                      opacity: "var(--avatar-border-opacity, 0)",
+                      transform: "var(--avatar-border-transform)",
+                    }}
+                  />
+                  <Avatar
+                    large
+                    className="block h-16 w-16 origin-left"
+                    style={{ transform: "var(--avatar-image-transform)" }}
+                  />
+                </div>
+              </div>
+            </Container>
+          </>
+        )}
+        <div
+          ref={headerRef}
+          className="top-0 z-10 h-16 pt-6"
+          style={{
+            position:
+              "var(--header-position)" as React.CSSProperties["position"],
+          }}
+        >
+          <Container
+            className="top-[var(--header-top,theme(spacing.6))] w-full"
+            style={{
+              position:
+                "var(--header-inner-position)" as React.CSSProperties["position"],
+            }}
+          >
+            <div className="relative flex gap-4">
+              <div className="flex flex-1">
+                {!isHomePage && (
+                  <AvatarContainer>
+                    <Avatar />
+                  </AvatarContainer>
+                )}
+              </div>
+              <div
+                id="mobile-nav-container"
+                className="flex flex-1 justify-end md:justify-center"
+              >
+                <MobileNavigation />
+                <DesktopNavigation className="pointer-events-auto hidden md:block" />
+              </div>
+              <div className="flex justify-end md:flex-1">
+                <div className="pointer-events-auto">
+                  <ModeToggle />
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </header>
+      {isHomePage && (
+        <div
+          className="flex-none"
+          style={{ height: "var(--content-offset)" }}
+        />
+      )}
+    </>
+  );
+};
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import clsx from "clsx";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
@@ -12,25 +13,56 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          variant="outline"
+          size="icon"
+          className="bg-white/90 text-zinc-800 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur aria-expanded:text-teal-500 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 aria-expanded:dark:text-teal-400"
+        >
+          <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+      <DropdownMenuContent
+        align="end"
+        className="rounded-md bg-white/90 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+      >
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className={clsx(
+            "text-sm font-medium",
+            theme === "light"
+              ? "text-teal-500 dark:text-teal-400"
+              : "text-zinc-800 dark:text-zinc-200"
+          )}
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className={clsx(
+            "text-sm font-medium",
+            theme === "dark"
+              ? "text-teal-500 dark:text-teal-400"
+              : "text-zinc-800 dark:text-zinc-200"
+          )}
+        >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          onClick={() => setTheme("system")}
+          className={clsx(
+            "text-sm font-medium",
+            theme === "system"
+              ? "text-teal-500 dark:text-teal-400"
+              : "text-zinc-800 dark:text-zinc-200"
+          )}
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
