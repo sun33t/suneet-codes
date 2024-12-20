c6d72514ac25303a29ada4c9e88864d207bee77b
Suneet Misra <hello@suneet.codes>
2024-12-20 14:54:47 +0000

    💄 Modifies Large component
    Converts Large component into polymorphic component in line with PR feedback

--- components/large.tsx
+++ components/large.tsx
@@ -1,10 +1,20 @@
 import { cn } from "@/lib/utils";
 
-type LargeProps = React.ComponentPropsWithoutRef<"div">;
-export const Large = ({ children, className, ...rest }: LargeProps) => {
+type LargeTags = "h1" | "h2" | "h3" | "h4" | "p";
+
+type LargeProps<C extends LargeTags> = {
+  as?: C;
+} & React.ComponentPropsWithoutRef<C>;
+export const Large = <C extends LargeTags>({
+  children,
+  className,
+  as,
+  ...rest
+}: LargeProps<C>) => {
+  const Component = as || "p";
   return (
-    <div className={cn("text-lg font-semibold", className)} {...rest}>
+    <Component className={cn("text-lg font-semibold", className)} {...rest}>
       {children}
-    </div>
+    </Component>
   );
 };
