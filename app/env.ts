780db1823d0b73a73897f0319bbf5495c57cd025
Suneet Misra <hello@suneet.codes>
2024-12-21 22:59:21 +0000

    🔧 Adds new values to env.ts

--- app/env.ts
+++ app/env.ts
@@ -4,8 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PROJECT_CANONICAL_URL: z.string().url(),
+    PROJECT_GITHUB_URL: z.string().url(),
+    PROJECT_LINKEDIN_URL: z.string().url(),
     PROJECT_AUTHOR: z.string(),
     PROJECT_BASE_TITLE: z.string(),
+    PROJECT_BASE_DESCRIPTION: z.string(),
     NODE_ENV: z.union([
       z.literal("development"),
       z.literal("production"),
