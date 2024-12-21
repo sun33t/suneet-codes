f0a341e62b8096e07b4057a84b8b294bbb45f9d7
Suneet Misra <hello@suneet.codes>
2024-12-21 23:01:18 +0000

    ✨ Adds hero section on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,59 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
+import { env } from "@/app/env";
+import { Container } from "@/components/container";
+import { GitHubIcon, LinkedInIcon } from "@/components/social-icons";
+import { Heading } from "@/components/typography/heading";
+import { P } from "@/components/typography/paragraph";
+
+function SocialLink({
+  icon: Icon,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Link> & {
+  icon: React.ComponentType<{ className?: string }>;
+}) {
+  return (
+    <Link className="group -m-1 p-1" {...props} target="_blank">
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-accent-foreground dark:fill-zinc-400 dark:group-hover:fill-accent-foreground" />
+    </Link>
+  );
+}
+
 export default function Home() {
-  return <div>Content</div>;
+  return (
+    <Fragment>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <Heading as={"h1"}>{env.PROJECT_AUTHOR}</Heading>
+          <P className="text-base text-muted-foreground">
+            👋, I&apos;m a developer and founder based in the UK. I&apos;ve been
+            building software in the{" "}
+            <span className="text-accent-foreground">e-commerce</span> and{" "}
+            <span className="text-accent-foreground">ombudsman</span> spaces for
+            over five years after completing my MSc in Computing. When I&apos;m
+            not working, I like to cook and travel with my wife.
+          </P>
+          <div className="mt-6 flex gap-2">
+            {/* <SocialLink href="#" aria-label="Follow on X" icon={XIcon} /> */}
+            {/* <SocialLink
+              href="#"
+              aria-label="Follow on Instagram"
+              icon={InstagramIcon}
+            /> */}
+            <SocialLink
+              href={env.PROJECT_GITHUB_URL}
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+            />
+            <SocialLink
+              href={env.PROJECT_LINKEDIN_URL}
+              aria-label="Follow on LinkedIn"
+              icon={LinkedInIcon}
+            />
+          </div>
+        </div>
+      </Container>
+    </Fragment>
+  );
 }
