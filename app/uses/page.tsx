2180ebec35c3738824b8d3dfcda77f5d238e4295
Suneet Misra <hello@suneet.codes>
2024-12-22 08:01:23 +0000

    💄 Removes redundant spacings

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -3,7 +3,6 @@ import { Container } from "@/components/container";
 export default function Uses() {
   return (
     <Container id="uses-page-container" className="mt-9">
-      {" "}
       <div className="flex items-center justify-center">Uses Page</div>
     </Container>
   );
