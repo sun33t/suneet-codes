a01132420220e7980dd135c384f71b5e5a9280b0
Suneet Misra <hello@suneet.codes>
2024-12-22 08:01:23 +0000

    💄 Removes redundant spacings

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,6 @@ import { Container } from "@/components/container";
 export default function Projects() {
   return (
     <Container id="projects-page-container" className="mt-9">
-      {" "}
       <div className="flex items-center justify-center">Projects Page</div>
     </Container>
   );
