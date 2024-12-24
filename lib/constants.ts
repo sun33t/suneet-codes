02fa74dfaacd40905315ad41b8c0aa60f1487dd3
Suneet Misra <hello@suneet.codes>
2024-12-24 15:55:52 +0000

    🎨 Updates typings for article categories

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,11 +10,44 @@ export const PAGES: Pages = [
   { title: "Uses", slug: "/uses" },
 ];
 
+export const categoryNames = ["react", "javascript", "aws"] as const;
+
+export type CategoryName = (typeof categoryNames)[number];
+
+export type Category = {
+  title: CategoryName;
+  slug: string;
+  className: string;
+};
+
+type Categories = Map<CategoryName, Category>;
+
 /**
  * An array of categories for blog posts and articles.
  */
-export const CATEGORIES: Array<{ title: string }> = [
-  { title: "react" },
-  { title: "javascript" },
-  { title: "aws" },
-];
+export const CATEGORIES: Categories = new Map([
+  [
+    "react",
+    {
+      title: "react",
+      slug: "react",
+      className: "bg-red-400 text-white dark:bg-red-200 dark:text-black",
+    },
+  ],
+  [
+    "javascript",
+    {
+      title: "javascript",
+      slug: "javascript",
+      className: "bg-green-400 text-white dark:bg-green-200 dark:text-black",
+    },
+  ],
+  [
+    "aws",
+    {
+      title: "aws",
+      slug: "aws",
+      className: "bg-yellow-400 dark:bg-yellow-200 text-black",
+    },
+  ],
+]);
