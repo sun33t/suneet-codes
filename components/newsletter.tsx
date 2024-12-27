356184b7a206547dbaa6aeb0c4b2daf0ae6789ec
Suneet Misra <hello@suneet.codes>
2024-12-27 13:00:52 +0000

    🎨 Completes v1 of homepage
    newsletter and resume components are moved into their own modules and newsletter section is temporarily removed on homepage until the functionality is built for it.

---
+++ components/newsletter.tsx
@@ -0,0 +1,32 @@
+import { MailIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export const Newsletter = () => {
+  return (
+    <form
+      action="/thank-you"
+      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+    >
+      <h2 className="flex items-center text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <MailIcon className="h-5 w-5 flex-none fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500" />
+        <span className="ml-3">Stay up to date</span>
+      </h2>
+      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        Get notified when I publish something new, and unsubscribe at any time.
+      </p>
+      <div className="mt-6 flex">
+        <input
+          type="email"
+          placeholder="Email address"
+          aria-label="Email address"
+          required
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+        />
+        <Button type="submit" className="ml-4 flex-none">
+          Join
+        </Button>
+      </div>
+    </form>
+  );
+};
