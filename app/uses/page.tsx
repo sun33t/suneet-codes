3762ed2cc548e525966ab0b494c84cc7aa031345
Suneet Misra <hello@suneet.codes>
2025-01-01 09:24:27 +0000

    💬 Adds more content to /following page

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -2,6 +2,8 @@ import { Card } from "@/components/card";
 import { Section } from "@/components/section";
 import { SimpleLayout } from "@/components/simple-layout";
 
+// https://www.robinwieruch.de/about/ look here for inspo
+
 const ToolsSection = ({
   children,
   ...props
