aaacc77c7bcb7d36554a3a621b914ecac93ee2fd
Suneet Misra <hello@suneet.codes>
2025-01-01 09:54:44 +0000

    💡 Removes commented out code

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -10,17 +10,6 @@ import {
 } from "@/components/ui/accordion";
 import { INSPIRATION, InspirationEntry } from "@/lib/constants";
 
-// const InspirationSection = ({
-//   children,
-//   ...props
-// }: React.ComponentPropsWithoutRef<typeof Section>) => {
-//   return (
-//     <Section {...props}>
-//       <div className="space-y-16">{children}</div>
-//     </Section>
-//   );
-// };
-
 type InspirationProps = InspirationEntry;
 
 const Inspiration = ({ title, description, cta, href }: InspirationProps) => {
@@ -29,7 +18,6 @@ const Inspiration = ({ title, description, cta, href }: InspirationProps) => {
       <Card.Title as="h4" href={href}>
         {title}
       </Card.Title>
-      {/* <Card.Eyebrow decorate>{event}</Card.Eyebrow> */}
       <Card.Description>{description}</Card.Description>
       <Card.Cta>{cta}</Card.Cta>
     </Card>
@@ -47,19 +35,6 @@ export default function Following() {
       title="Developers and creative professionals whose work I follow."
       intro="This industry is always changing and there's always new challenges to overcome. These are the people who I find inspiring and invaluable to learn from."
     >
-      {/* <div className="space-y-20">
-        {Array.from(
-          INSPIRATION.keys().map((category) => {
-            return (
-              <InspirationSection key={category} title={category}>
-                {INSPIRATION.get(category)?.map((entry) => (
-                  <Inspiration key={entry.href} {...entry} />
-                ))}
-              </InspirationSection>
-            );
-          })
-        )}
-      </div> */}
       <div className="space-y-20">
         <Accordion type="single" collapsible className="max-w-md">
           {Array.from(
