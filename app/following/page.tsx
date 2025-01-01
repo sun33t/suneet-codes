9bed5b77c27e74936a32c556c9ef30c8b530e32c
Suneet Misra <hello@suneet.codes>
2025-01-01 18:20:15 +0000

    ➖ Removes shadcn accordion

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -1,23 +1,30 @@
 import { Metadata } from "next";
 
 import { Card } from "@/components/card";
+import { Section } from "@/components/section";
 import { SimpleLayout } from "@/components/simple-layout";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { INSPIRATION, InspirationEntry } from "@/lib/constants";
+
+const InspirationSection = ({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Section>) => {
+  return (
+    <Section {...props}>
+      <div className="space-y-16">{children}</div>
+    </Section>
+  );
+};
 
 type InspirationProps = InspirationEntry;
 
 const Inspiration = ({ title, description, cta, href }: InspirationProps) => {
   return (
-    <Card as="article" className="px-4">
-      <Card.Title as="h4" href={href}>
+    <Card as="article">
+      <Card.Title as="h3" href={href}>
         {title}
       </Card.Title>
+      {/* <Card.Eyebrow decorate>{event}</Card.Eyebrow> */}
       <Card.Description>{description}</Card.Description>
       <Card.Cta>{cta}</Card.Cta>
     </Card>
@@ -36,26 +43,17 @@ export default function Following() {
       intro="This industry is always changing and there's always new challenges to overcome. These are the people who I find inspiring and invaluable to learn from."
     >
       <div className="space-y-20">
-        <Accordion type="single" collapsible className="max-w-md">
-          {Array.from(
-            INSPIRATION.keys().map((category) => {
-              return (
-                <AccordionItem key={category} value={category}>
-                  <AccordionTrigger className="text-base font-bold hover:no-underline">
-                    {category}
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div className="flex flex-col space-y-16">
-                      {INSPIRATION.get(category)?.map((entry) => (
-                        <Inspiration key={entry.href} {...entry} />
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            })
-          )}
-        </Accordion>
+        {Array.from(
+          INSPIRATION.keys().map((category) => {
+            return (
+              <InspirationSection key={category} title={category}>
+                {INSPIRATION.get(category)?.map((entry) => (
+                  <Inspiration key={entry.href} {...entry} />
+                ))}
+              </InspirationSection>
+            );
+          })
+        )}
       </div>
     </SimpleLayout>
   );
