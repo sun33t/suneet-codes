d8a47df5200b8d328d8b6d28eb4d562163d702f7
Suneet Misra <hello@suneet.codes>
2025-01-02 21:19:57 +0000

    🎨 Adds reusable NotFound component

--- app/articles/[slug]/not-found.tsx
+++ app/articles/[slug]/not-found.tsx
@@ -1,33 +1,18 @@
 import { Metadata } from "next";
-import Link from "next/link";
 
-import { Container } from "@/components/container";
-import { buttonVariants } from "@/components/ui/button";
+import { NotFound } from "@/components/not-found";
 
 export const metadata: Metadata = {
   title: "404 - Article not found",
 };
 
-export default function NotFound() {
+export default function NotFoundPage() {
   return (
-    <Container className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold">404</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight sm:text-7xl">
-          Article not found
-        </h1>
-        <p className="mt-6 text-pretty text-lg font-medium text-muted-foreground sm:text-xl/8">
-          Sorry, we couldn’t find the article you’re looking for.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/articles"
-            className={buttonVariants({ variant: "default" })}
-          >
-            Go back to articles
-          </Link>
-        </div>
-      </div>
-    </Container>
+    <NotFound
+      title="Article not found"
+      message="Sorry, we couldn’t find the article you’re looking for."
+      buttonText="Go back to articles"
+      buttonHref="/articles"
+    />
   );
 }
