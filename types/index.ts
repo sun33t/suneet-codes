c672fe5606f6664f4c877891401bf4a7aa21733d
Suneet Misra <hello@suneet.codes>
2025-01-03 11:05:53 +0000

    🎨 Bootstraps /contact page

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,12 @@
 import { type Article } from "content-collections";
 
-export type Page = "about" | "articles" | "projects" | "following" | "uses";
+export type Page =
+  | "about"
+  | "articles"
+  | "projects"
+  | "following"
+  | "uses"
+  | "contact";
 export type PageTitle = Record<"title", Page>;
 
 export type SearchParams = Promise<{
