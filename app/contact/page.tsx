4ec765934f1d1848b4b8eee53a99e3ac8c14174e
Suneet Misra <hello@suneet.codes>
2025-01-05 01:28:30 +0000

    💄 Updates styles on /contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,7 @@ import { env } from "@/app/env";
 import { ContactForm } from "@/components/contact-form";
 import { SimpleLayout } from "@/components/simple-layout";
 import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { PAGE_METADATA } from "@/content/pages";
 
 export const metadata: Metadata = { ...PAGE_METADATA.contact };
@@ -16,26 +17,30 @@ export default function Contact() {
       title="Contact"
       intro={`Book a time to catch up in my calendar, send me an email or, if you prefer, fill in an enquiry form and I'll come back to you as soon as I can.`}
     >
+      <Card className="mx-auto mb-16 max-w-lg bg-white px-6 py-6 dark:bg-zinc-900">
+        <CardContent className="pb-0">
+          <div className="grid max-w-lg grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <Link
+              aria-label="Book some time in my calendar to catch up."
+              href={env.PROJECT_CALENDAR_URL}
+              target="_blank"
+              className={`w-full ${buttonVariants({ variant: "secondary" })}`}
+            >
+              Calendar
+              <CalendarPlus2 className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+            </Link>
+            <Link
+              aria-label="Send me an email."
+              href={`mailto:${env.PROJECT_EMAIL_ADDRESS}`}
+              className={`w-full ${buttonVariants({ variant: "secondary" })}`}
+            >
+              Email
+              <MailIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
       <ContactForm />
-      <div className="grid max-w-lg grid-cols-1 gap-x-8 sm:grid-cols-2">
-        <Link
-          aria-label="Book some time in my calendar to catch up."
-          href={env.PROJECT_CALENDAR_URL}
-          target="_blank"
-          className={`mt-6 w-full ${buttonVariants({ variant: "secondary" })}`}
-        >
-          Calendar
-          <CalendarPlus2 className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-        </Link>
-        <Link
-          aria-label="Send me an email."
-          href={`mailto:${env.PROJECT_EMAIL_ADDRESS}`}
-          className={`mt-6 w-full ${buttonVariants({ variant: "secondary" })}`}
-        >
-          Email
-          <MailIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-        </Link>
-      </div>
     </SimpleLayout>
   );
 }
--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -11,6 +11,7 @@ import { useActionState, useEffect, useState } from "react";
 import { createEnquiry } from "@/app/contact/action";
 import { env } from "@/app/env";
 import { Turnstile } from "@/components/turnstile";
+import { Card, CardContent } from "@/components/ui/card";
 import { useToast } from "@/hooks/use-toast";
 
 const SubmitButton = ({
@@ -80,100 +81,104 @@ export const ContactForm = () => {
     dispatch(sanitizedFormData);
   };
   return (
-    <form action={formAction} className="max-w-lg">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2">
-        <div id="form-group-firstname">
-          <Label htmlFor="firstname">First name</Label>
-          <div className="mt-2.5">
-            <Input
-              defaultValue={state?.fields?.firstname}
-              id="firstname"
-              name="firstname"
-              type="text"
-              autoComplete="given-name"
-              placeholder="Mickey"
-              required
-              minLength={3}
+    <Card className="mx-auto max-w-lg bg-white dark:bg-zinc-900">
+      <CardContent className="px-12 py-6">
+        <form action={formAction} className="max-w-lg">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2">
+            <div id="form-group-firstname">
+              <Label htmlFor="firstname">First name</Label>
+              <div className="mt-2.5">
+                <Input
+                  defaultValue={state?.fields?.firstname}
+                  id="firstname"
+                  name="firstname"
+                  type="text"
+                  autoComplete="given-name"
+                  placeholder="Mickey"
+                  required
+                  minLength={3}
+                />
+                <ErrorMessage error={state?.errors?.firstname} />
+              </div>
+            </div>
+            <div id="form-group-lastname">
+              <Label htmlFor="lastname">Last name</Label>
+              <div className="mt-2.5">
+                <Input
+                  defaultValue={state?.fields?.lastname}
+                  id="lastname"
+                  name="lastname"
+                  type="text"
+                  autoComplete="family-name"
+                  placeholder="Mouse"
+                  required
+                  minLength={3}
+                />
+                <ErrorMessage error={state?.errors?.lastname} />
+              </div>
+            </div>
+            <div id="form-group-company" className="sm:col-span-2">
+              <Label htmlFor="company">Company</Label>
+              <div className="mt-2.5">
+                <Input
+                  defaultValue={state?.fields?.company}
+                  id="company"
+                  name="company"
+                  type="text"
+                  autoComplete="organization"
+                  placeholder="Acme Corp"
+                />
+                <ErrorMessage error={state?.errors?.company} />
+              </div>
+            </div>
+            <div id="form-group-email" className="sm:col-span-2">
+              <Label htmlFor="email">Email</Label>
+              <div className="mt-2.5">
+                <Input
+                  defaultValue={state?.fields?.email}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="mickey@acme.com"
+                  required
+                />
+                <ErrorMessage error={state?.errors?.email} />
+              </div>
+            </div>
+            <div id="form-group-message" className="sm:col-span-2">
+              <Label htmlFor="message">Message</Label>
+              <div className="mt-2.5">
+                <Textarea
+                  defaultValue={state?.fields?.message}
+                  id="message"
+                  name="message"
+                  rows={4}
+                  placeholder="Let's work together!"
+                  required
+                  minLength={10}
+                />
+                <ErrorMessage error={state?.errors?.message} />
+              </div>
+            </div>
+            <Turnstile
+              siteKey={env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+              onVerify={setToken}
+              appearance="always"
+              theme="auto"
             />
-            <ErrorMessage error={state?.errors?.firstname} />
+            <div id="form-group-submit" className="sm:col-span-2">
+              <SubmitButton pending={isPending} disabled={!token} />
+            </div>
           </div>
-        </div>
-        <div id="form-group-lastname">
-          <Label htmlFor="lastname">Last name</Label>
-          <div className="mt-2.5">
-            <Input
-              defaultValue={state?.fields?.lastname}
-              id="lastname"
-              name="lastname"
-              type="text"
-              autoComplete="family-name"
-              placeholder="Mouse"
-              required
-              minLength={3}
-            />
-            <ErrorMessage error={state?.errors?.lastname} />
-          </div>
-        </div>
-        <div id="form-group-company" className="sm:col-span-2">
-          <Label htmlFor="company">Company</Label>
-          <div className="mt-2.5">
-            <Input
-              defaultValue={state?.fields?.company}
-              id="company"
-              name="company"
-              type="text"
-              autoComplete="organization"
-              placeholder="Acme Corp"
-            />
-            <ErrorMessage error={state?.errors?.company} />
-          </div>
-        </div>
-        <div id="form-group-email" className="sm:col-span-2">
-          <Label htmlFor="email">Email</Label>
-          <div className="mt-2.5">
-            <Input
-              defaultValue={state?.fields?.email}
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              placeholder="mickey@acme.com"
-              required
-            />
-            <ErrorMessage error={state?.errors?.email} />
-          </div>
-        </div>
-        <div id="form-group-message" className="sm:col-span-2">
-          <Label htmlFor="message">Message</Label>
-          <div className="mt-2.5">
-            <Textarea
-              defaultValue={state?.fields?.message}
-              id="message"
-              name="message"
-              rows={4}
-              placeholder="Let's work together!"
-              required
-              minLength={10}
-            />
-            <ErrorMessage error={state?.errors?.message} />
-          </div>
-        </div>
-        <Turnstile
-          siteKey={env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
-          onVerify={setToken}
-          appearance="always"
-          theme="auto"
-        />
-        <div id="form-group-submit" className="sm:col-span-2">
-          <SubmitButton pending={isPending} disabled={!token} />
-        </div>
-      </div>
 
-      <p aria-live="polite" className="sr-only" role="status">
-        {state?.success
-          ? "Successful submission"
-          : "Unsuccessful submission, please try again"}
-      </p>
-    </form>
+          <p aria-live="polite" className="sr-only" role="status">
+            {state?.success
+              ? "Successful submission"
+              : "Unsuccessful submission, please try again"}
+          </p>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
