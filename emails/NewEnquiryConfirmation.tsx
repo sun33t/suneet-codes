43a3ec44e003e8b2a1dd1ea9a9c635d1dc192687
Suneet Misra <hello@suneet.codes>
2025-01-06 16:55:34 +0000

    🎨 Addresses issues with email flow

--- emails/NewEnquiryConfirmation.tsx
+++ emails/NewEnquiryConfirmation.tsx
@@ -17,8 +17,8 @@ interface NewEnquiryConfirmationEmailProps {
   firstname: string;
 }
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : "http://localhost:3000";
 
 const NewEnquiryConfirmationEmail = ({
