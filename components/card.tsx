67ef5f177793fd272171569fc520856b8e1263f0
Suneet Misra <hello@suneet.codes>
2025-01-07 08:17:39 +0000

    🎨 Adds hover colour to card titles on /uses page

--- components/card.tsx
+++ components/card.tsx
@@ -53,15 +53,22 @@ Card.Title = function CardTitle<T extends React.ElementType = "h2">({
   href,
   children,
   isExternal = false,
+  className,
 }: Omit<React.ComponentPropsWithoutRef<T>, "as" | "href"> & {
   as?: T;
   href?: string;
   isExternal?: boolean;
+  className?: string;
 }) {
   const Component = as ?? "h2";
 
   return (
-    <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+    <Component
+      className={clsx(
+        className,
+        "text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100"
+      )}
+    >
       {href ? (
         <Card.Link
           href={href}
