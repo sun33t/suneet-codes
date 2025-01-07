bfa2713e3f325ac105a0b647af1e5df3a1c83765
Suneet Misra <hello@suneet.codes>
2025-01-07 22:32:10 +0000

    🎨 Article categories have single source of truth

--- content/categories.ts
+++ content/categories.ts
@@ -1,25 +1,24 @@
-export type ArticleCategory = {
-  title: "react" | "javascript" | "aws" | "leadership";
+import slugify from "slugify";
+
+// CATEGORYTITLES determines the validation schama for category tags within article frontmatter, as well as forming the source for slugs in the CATEGORYWITHSLUGS array. This is the single source of truth for legal categories throughout the application.
+export const CATEGORYTITLES = [
+  "aws",
+  "react",
+  "javascript",
+  "leadership",
+  "another category",
+] as const;
+
+type CategorytitleWithSlug = {
+  title: (typeof CATEGORYTITLES)[number];
   slug: string;
 };
 
-export const CATEGORIES: ArticleCategory[] = [
-  {
-    title: "react",
-    slug: "react",
-  },
-  {
-    title: "javascript",
-    slug: "javascript",
-  },
-  {
-    title: "aws",
-    slug: "aws",
-  },
-  {
-    title: "leadership",
-    slug: "leadership",
-  },
-];
+export const CATEGORYWITHSLUGS: CategorytitleWithSlug[] = CATEGORYTITLES.map(
+  (title) => ({
+    title,
+    slug: slugify(title, { lower: true }),
+  })
+);
 
 export const CATEGORY_PARAM_NAME = "category";
