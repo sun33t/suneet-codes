a8ec145e5a5553694ca6dd68f25193bbee469e29
Suneet Misra <hello@suneet.codes>
2025-01-07 18:37:06 +0000

    🎨 Adds manifest file

---
+++ app/manifest.ts
@@ -0,0 +1,52 @@
+import type { MetadataRoute } from "next";
+
+import { env } from "@/app/env";
+import { baseUrl } from "@/lib/baseUrl";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: env.PROJECT_BASE_TITLE,
+    short_name: env.PROJECT_BASE_TITLE,
+    description: env.PROJECT_BASE_DESCRIPTION,
+    orientation: "portrait",
+    start_url: "/",
+    display: "standalone",
+    icons: [
+      {
+        src: "/apple-icon.png",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+    categories: [
+      "software",
+      "development",
+      "app development",
+      "developer",
+      "software engineering",
+      "web development",
+      "programming",
+      "coding",
+      "web design",
+    ],
+    scope: baseUrl.href,
+    shortcuts: [
+      {
+        name: "About",
+        url: "/about",
+      },
+      {
+        name: "Articles",
+        url: "/articles",
+      },
+      {
+        name: "Contact",
+        url: "/contact",
+      },
+      {
+        name: "Projects",
+        url: "/projects",
+      },
+    ],
+  };
+}
