287c375c90be42d4a1d4768eb009af446bff046c
Suneet Misra <hello@suneet.codes>
2025-01-07 20:20:43 +0000

    🎨 Adds server-side implementation of posthog

---
+++ lib/posthog.ts
@@ -0,0 +1,17 @@
+/**
+ * This is the server side implementation of posthog which is analagous to using usePosthog (provided by the @/components/posthog-provider) on the client side.
+ */
+import { PostHog } from "posthog-node";
+
+import { env } from "@/app/env";
+
+const PostHogClient = () => {
+  const posthogClient = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0,
+  });
+  return posthogClient;
+};
+
+export default PostHogClient;
