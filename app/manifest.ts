0e3d9d51481755754396fe67b1d89efa684f58c9
Suneet Misra <hello@suneet.codes>
2025-01-07 18:40:19 +0000

    🎨 Corrects image type

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: "/apple-icon.png",
         sizes: "any",
-        type: "image/x-icon",
+        type: "image/png",
       },
     ],
     categories: [
