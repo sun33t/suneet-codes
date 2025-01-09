6c67a485287d380df8200097c5e2437d68a2f092
Suneet Misra <hello@suneet.codes>
2025-01-09 13:27:18 +0000

    🎨 Adds link-card to /following page

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -1,7 +1,14 @@
 import { Metadata } from "next";
-import Link from "next/link";
 import { useMemo } from "react";
 
+import {
+  LinkCard,
+  LinkCardContentContainer,
+  LinkCardDescription,
+  LinkCardFooter,
+  LinkCardHeader,
+  LinkCardTitle,
+} from "@/components/link-card";
 import { SimpleLayout } from "@/components/simple-layout";
 import {
   Accordion,
@@ -9,13 +16,6 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { FOLLOWING, FollowingEntry } from "@/content/following";
 import { PAGE_METADATA } from "@/content/pages";
 
@@ -23,25 +23,17 @@ export const metadata: Metadata = { ...PAGE_METADATA.following };
 
 const FollowingCard = ({ title, description, cta, href }: FollowingEntry) => {
   return (
-    <Card className="group relative border-none bg-transparent text-foreground shadow-none">
-      <div className="absolute -inset-x-4 -bottom-0 -top-6 z-0 scale-95 rounded-2xl bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 dark:bg-zinc-800/50" />
-      <Link href={href} target="_blank" rel="noopener noreferrer">
-        <span className="relative z-10">
-          <CardHeader className="space-y-3 p-0">
-            <CardTitle>{title}</CardTitle>
-            <CardDescription
-              id="card-description"
-              aria-label={`About ${title}`}
-            >
-              {description}
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="pl-0 pt-4">
-            <p className="text-sm text-accent-foreground">{`${cta} >`}</p>
-          </CardFooter>
-        </span>
-      </Link>
-    </Card>
+    <LinkCard href={href} isExternal={true}>
+      <LinkCardHeader>
+        <LinkCardTitle>{title}</LinkCardTitle>
+      </LinkCardHeader>
+      <LinkCardContentContainer>
+        <LinkCardDescription>{description}</LinkCardDescription>
+      </LinkCardContentContainer>
+      <LinkCardFooter>
+        <p className="text-sm text-accent-foreground">{`${cta} >`}</p>
+      </LinkCardFooter>
+    </LinkCard>
   );
 };
 
