76cbee892145f83ef73e68ecba377357f8c40047
Suneet Misra <hello@suneet.codes>
2025-01-10 07:53:16 +0000

    🎨 Adds args validation

--- components/cloudinary-image.tsx
+++ components/cloudinary-image.tsx
@@ -3,5 +3,9 @@
 import { CldImage, type CldImageProps } from "next-cloudinary";
 
 export const CloudinaryImage = (props: CldImageProps) => {
+  if (!props.src || !props.alt) {
+    console.warn("CloudinaryImage: missing required props src and/or alt.");
+  }
+
   return <CldImage {...props} />;
 };
