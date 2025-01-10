6c2aeec1bbb680c207ccfbe6acc16f198513463e
Suneet Misra <hello@suneet.codes>
2025-01-10 07:53:54 +0000

    🎨 Adds caching mechanism

--- lib/utils/getCloudinaryBlurDataUrl.ts
+++ lib/utils/getCloudinaryBlurDataUrl.ts
@@ -1,16 +1,24 @@
 "use server";
 
-import { getCldImageUrl } from "next-cloudinary";
+import { type GetCldImageUrlOptions, getCldImageUrl } from "next-cloudinary";
 
-export const getCloudinaryBlurDataUrl = async (src: string) => {
-  const imageUrl = getCldImageUrl({
-    src,
-    width: "65ch",
-  });
+const blurDataUrlCache = new Map<string, string>();
+
+export const getCloudinaryBlurDataUrl = async (
+  options: GetCldImageUrlOptions
+) => {
+  if (blurDataUrlCache.has(options.src)) {
+    return blurDataUrlCache.get(options.src);
+  }
+
+  const imageUrl = getCldImageUrl(options);
   const cloudinaryResponse = await fetch(imageUrl);
   const arrayBuffer = await cloudinaryResponse.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
   const base64 = buffer.toString("base64");
   const dataUrl = `data:${cloudinaryResponse.type};base64,${base64}`;
+
+  blurDataUrlCache.set(options.src, dataUrl);
+
   return dataUrl;
 };
