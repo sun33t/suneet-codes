33d009209fe79f49bf462e27553b346acfe93b30
Suneet Misra <hello@suneet.codes>
2025-01-11 09:17:04 +0000

    🎨 Adds docs to getCloudinaryBlurDataUrl

--- lib/utils/getCloudinaryBlurDataUrl.ts
+++ lib/utils/getCloudinaryBlurDataUrl.ts
@@ -9,8 +9,10 @@ const blurDataUrlCache = new Map<string, string>();
 export const getCloudinaryBlurDataUrl = async (
   options: GetCldImageUrlOptions
 ): Promise<{ blurDataUrl: string | undefined; imageSrc: string }> => {
+  // create cloudinary image path
   const imageSrc = `${env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/${options.src}`;
 
+  // check cache first for existing blurDataUrl
   if (blurDataUrlCache.has(imageSrc)) {
     return {
       blurDataUrl: blurDataUrlCache.get(imageSrc),
@@ -18,8 +20,10 @@ export const getCloudinaryBlurDataUrl = async (
     };
   }
 
+  // no cache found, compile cloudinary image url
   const imageUrl = getCldImageUrl({ ...options, src: imageSrc });
 
+  // fetch image information from cloudinary
   const cloudinaryResponse = await fetch(imageUrl);
 
   // if (Object.keys(cloudinaryResponse).length === 0) {
@@ -28,12 +32,16 @@ export const getCloudinaryBlurDataUrl = async (
   //   );
   //   return null;
   // }
+
+  // convert response to arrayBuffer and then to base64 string
   const arrayBuffer = await cloudinaryResponse.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
   const base64 = buffer.toString("base64");
   const dataUrl = `data:${cloudinaryResponse.type};base64,${base64}`;
 
+  // cache the generated blurDataUrl for future use
   blurDataUrlCache.set(imageSrc, dataUrl);
 
+  // return the generated blurDataUrl and the compiled image path
   return { blurDataUrl: dataUrl, imageSrc };
 };
