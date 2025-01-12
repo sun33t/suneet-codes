e5cc7ede34b8e79545df98d19cf3e9457e7fd0b4
Suneet Misra <hello@suneet.codes>
2025-01-12 16:27:13 +0000

    🎨 Adds PageHeading to home page

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import {
   LinkCardLabel,
   LinkCardTitle,
 } from "@/components/link-card";
+import { PageHeading } from "@/components/page-heading";
 import { Resume } from "@/components/resume";
 import { GitHubIcon, LinkedInIcon } from "@/components/social-icons";
-import { Heading } from "@/components/typography/heading";
 import { P } from "@/components/typography/paragraph";
 import { ROLES } from "@/content/roles";
 import { latestArticles } from "@/lib/articles";
@@ -68,45 +68,46 @@ export default function Home() {
     <Fragment>
       <Container className="mt-12 duration-1000 animate-in fade-in">
         <div className="max-w-2xl">
-          <Heading as={"h1"}>{env.PROJECT_AUTHOR}</Heading>
-          <P className="text-base text-muted-foreground">
-            👋, I&apos;m a developer and founder based in the UK. I&apos;ve been
-            building software in the{" "}
-            <span className="font-semibold underline">e-commerce</span>,{" "}
-            <span className="font-semibold underline">retail</span> and{" "}
-            <span className="font-semibold underline">hospitality</span> spaces
-            as well as for the{" "}
-            <span className="font-semibold underline">
-              energy and comms ombudsman
-            </span>{" "}
-            over a five year period after completing my{" "}
-            <span className="font-semibold underline">MSc in Computing</span>.
-            When I&apos;m not working, I like to cook and travel with my wife
-            and spend time in the outdoors with my friends.
-          </P>
-          <div className="mt-6 flex gap-2">
-            {/* <SocialLink href="#" aria-label="Follow on X" icon={XIcon} /> */}
-            {/* <SocialLink
+          <PageHeading title={env.PROJECT_AUTHOR}>
+            <P className="text-muted-foreground">
+              👋, I&apos;m a developer and founder based in the UK. I&apos;ve
+              been building software in the{" "}
+              <span className="font-semibold underline">e-commerce</span>,{" "}
+              <span className="font-semibold underline">retail</span> and{" "}
+              <span className="font-semibold underline">hospitality</span>{" "}
+              spaces as well as for the{" "}
+              <span className="font-semibold underline">
+                energy and comms ombudsman
+              </span>{" "}
+              over a five year period after completing my{" "}
+              <span className="font-semibold underline">MSc in Computing</span>.
+              When I&apos;m not working, I like to cook and travel with my wife
+              and spend time in the outdoors with my friends.
+            </P>
+            <div className="mt-6 flex gap-2">
+              {/* <SocialLink href="#" aria-label="Follow on X" icon={XIcon} /> */}
+              {/* <SocialLink
               href="#"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             /> */}
-            <SocialLink
-              href={env.PROJECT_GITHUB_URL}
-              aria-label="Follow on GitHub"
-              icon={GitHubIcon}
-            />
-            <SocialLink
-              href={env.PROJECT_LINKEDIN_URL}
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            />
-          </div>
+              <SocialLink
+                href={env.PROJECT_GITHUB_URL}
+                aria-label="Follow on GitHub"
+                icon={GitHubIcon}
+              />
+              <SocialLink
+                href={env.PROJECT_LINKEDIN_URL}
+                aria-label="Follow on LinkedIn"
+                icon={LinkedInIcon}
+              />
+            </div>
+          </PageHeading>
         </div>
       </Container>
-      <Container className="mt-24 md:mt-28">
+      <Container className="mt-24 duration-1000 animate-in fade-in md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
-          <div className="duration-1000 animate-in fade-in">
+          <div className="">
             <div className="flex flex-col gap-16">
               {latestArticles.map((article) => {
                 return (
@@ -117,7 +118,7 @@ export default function Home() {
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             {/* <Newsletter /> */}
-            <div className="duration-1000 animate-in fade-in">
+            <div>
               <Resume roles={ROLES} />
             </div>
           </div>
