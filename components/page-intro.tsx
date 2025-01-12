bbaa969aa682a200e2221164324a51af49666ddb
Suneet Misra <hello@suneet.codes>
2025-01-12 17:10:23 +0000

    🎨 Multipe changes following pr feedback

--- components/page-intro.tsx
+++ components/page-intro.tsx
@@ -8,7 +8,10 @@ export const PageIntro = ({ title, children }: PageHeadingProps) => {
   return (
     <div>
       <header className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+        <h1
+          className="text-4xl font-bold tracking-tight sm:text-5xl"
+          aria-label={`${title} page`}
+        >
           {title}
         </h1>
         <div className="mt-6 text-base">{children}</div>
