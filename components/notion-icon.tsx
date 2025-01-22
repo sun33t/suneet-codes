6095f9a31595ab313d48d253e2e8b1909312770c
Suneet Misra <hello@suneet.codes>
2025-01-22 11:15:42 +0000

    🎨 Updates socialLinks on homepage

--- components/notion-icon.tsx
+++ components/notion-icon.tsx
@@ -5,7 +5,6 @@ export const NotionIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     preserveAspectRatio="xMidYMid"
     viewBox="0 0 256 268"
-    transform="scale(0.8)"
     {...props}
   >
     <path
