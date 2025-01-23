3989ec1f78d78c29ecc5920245c72d23efce89fa
Suneet Misra <hello@suneet.codes>
2025-01-23 16:28:41 +0000

    🎨 Removes optimizations to mobile nav

--- components/header/mobile-navigation.tsx
+++ components/header/mobile-navigation.tsx
@@ -12,7 +12,7 @@ import clsx from "clsx";
 import { AlignRight, X } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ComponentPropsWithoutRef, memo, useMemo } from "react";
+import { ComponentPropsWithoutRef, memo } from "react";
 
 import { ROUTES } from "@/lib/routes";
 import { Route, RouteProperties } from "@/types";
@@ -20,58 +20,51 @@ import { Route, RouteProperties } from "@/types";
 type MobileNavigationButtonProps = ComponentPropsWithoutRef<typeof Button> & {
   isMenuOpen: boolean;
 };
-export const MobileNavigationButton = memo(
-  ({ isMenuOpen, onClick }: MobileNavigationButtonProps) => {
-    const button = useMemo(
-      () => (
-        <Button
-          variant="outline"
-          size="icon"
-          className="bg-white/90 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur hover:bg-white/90 hover:text-accent-foreground aria-expanded:text-accent-foreground md:hidden dark:bg-zinc-800/90 dark:ring-white/10 hover:dark:bg-zinc-800/90"
-          onClick={onClick}
-        >
-          {isMenuOpen ? (
-            <X className="h-[1.5rem] w-[1.5rem]" />
-          ) : (
-            <AlignRight className="h-[1.5rem] w-[1.5rem]" />
-          )}
-        </Button>
-      ),
-      [isMenuOpen, onClick]
-    );
-
-    return button;
-  }
-);
-
-MobileNavigationButton.displayName = "MobileNavigationButton";
+export const MobileNavigationButton = ({
+  isMenuOpen,
+  onClick,
+}: MobileNavigationButtonProps) => {
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      className="bg-white/90 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur hover:bg-white/90 hover:text-accent-foreground aria-expanded:text-accent-foreground md:hidden dark:bg-zinc-800/90 dark:ring-white/10 hover:dark:bg-zinc-800/90"
+      onClick={onClick}
+    >
+      {isMenuOpen ? (
+        <X className="h-[1.5rem] w-[1.5rem]" />
+      ) : (
+        <AlignRight className="h-[1.5rem] w-[1.5rem]" />
+      )}
+    </Button>
+  );
+};
 
 type MobileNavigationItemProps = {
   item: RouteProperties;
   isActive: boolean;
 };
 
-const MobileNavigationItem = memo(
-  ({ item, isActive }: MobileNavigationItemProps) => {
-    return (
-      <li
-        key={`${item.title}-mob`}
-        className={clsx(
-          "w-full py-2 pl-4",
-          isActive &&
-            "rounded-xl border-none bg-zinc-50 shadow-none dark:bg-zinc-800/50",
-          isActive && "text-accent-foreground"
-        )}
-      >
-        <SheetClose asChild className="block w-full">
-          <Link href={item.slug}>{item.title}</Link>
-        </SheetClose>
-      </li>
-    );
-  }
-);
-
-MobileNavigationItem.displayName = "MobileNavigationItem";
+const MobileNavigationItem = ({
+  item,
+  isActive,
+}: MobileNavigationItemProps) => {
+  return (
+    <li
+      key={`${item.title}-mob`}
+      className={clsx(
+        "w-full py-2 pl-4",
+        isActive &&
+          "rounded-xl border-none bg-zinc-50 shadow-none dark:bg-zinc-800/50",
+        isActive && "text-accent-foreground"
+      )}
+    >
+      <SheetClose asChild className="block w-full">
+        <Link href={item.slug}>{item.title}</Link>
+      </SheetClose>
+    </li>
+  );
+};
 
 type MobileNavigationMenuProps = {
   routeNames: Route[];
@@ -127,24 +120,26 @@ type MobileNavigationProps = ComponentPropsWithoutRef<typeof Sheet> & {
   mobileAvatar: React.ReactElement;
   routeNames: Route[];
 };
-export const MobileNavigation = memo(
-  ({ open, onOpenChange, routeNames, mobileAvatar }: MobileNavigationProps) => {
-    const pathname = usePathname();
-    const stableMobileAvatar = useMemo(() => mobileAvatar, [mobileAvatar]);
-    return (
-      <Sheet open={open} onOpenChange={onOpenChange}>
-        <SheetContent side="left" mobileAvatar={stableMobileAvatar}>
-          <SheetHeader>
-            <SheetTitle className="sr-only">Mobile Navigation Menu</SheetTitle>
-            <SheetDescription className="sr-only">
-              Mobile navigation menu
-            </SheetDescription>
-          </SheetHeader>
-          <MobileNavigationMenu pathname={pathname} routeNames={routeNames} />
-        </SheetContent>
-      </Sheet>
-    );
-  }
-);
+export const MobileNavigation = ({
+  open,
+  onOpenChange,
+  routeNames,
+  mobileAvatar,
+}: MobileNavigationProps) => {
+  const pathname = usePathname();
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent side="left" mobileAvatar={mobileAvatar}>
+        <SheetHeader>
+          <SheetTitle className="sr-only">Mobile Navigation Menu</SheetTitle>
+          <SheetDescription className="sr-only">
+            Mobile navigation menu
+          </SheetDescription>
+        </SheetHeader>
+        <MobileNavigationMenu pathname={pathname} routeNames={routeNames} />
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 MobileNavigation.displayName = "MobileNavigation";
