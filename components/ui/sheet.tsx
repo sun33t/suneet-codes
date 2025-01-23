689c2a2b4dfb30906c10ba1cd888458c1b62beb6
Suneet Misra <hello@suneet.codes>
2025-01-23 16:28:41 +0000

    🎨 Removes optimizations to mobile nav

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -22,29 +22,22 @@ const SheetClose = SheetPrimitive.Close;
 
 const SheetPortal = SheetPrimitive.Portal;
 
-const SheetOverlay = React.memo(
-  React.forwardRef<
-    React.ElementRef<typeof SheetPrimitive.Overlay>,
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
-  >(({ className, ...props }, ref) => {
-    const combinedClassName = React.useMemo(
-      () =>
-        cn(
-          "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-          className
-        ),
-      [className]
-    );
+const SheetOverlay = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Overlay>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
+>(({ className, ...props }, ref) => {
+  return (
+    <SheetPrimitive.Overlay
+      className={cn(
+        "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        className
+      )}
+      {...props}
+      ref={ref}
+    />
+  );
+});
 
-    return (
-      <SheetPrimitive.Overlay
-        className={combinedClassName}
-        {...props}
-        ref={ref}
-      />
-    );
-  })
-);
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
@@ -76,56 +69,51 @@ const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
 >(({ side = "right", className, children, mobileAvatar, ...props }, ref) => {
-  const memoizedSheetPortal = React.useMemo(
-    () => (
-      <SheetPortal>
-        <SheetOverlay />
-        <SheetPrimitive.Content
-          ref={ref}
-          className={cn(sheetVariants({ side }), className)}
-          {...props}
-        >
-          <div className="flex flex-row items-center justify-between">
-            <SheetPrimitive.Close asChild>
-              <Link href="/">{mobileAvatar}</Link>
-            </SheetPrimitive.Close>
-            <SheetPrimitive.Close asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="bg-white/90 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur hover:bg-white/90 hover:text-accent-foreground aria-expanded:text-accent-foreground md:hidden dark:bg-zinc-800/90 dark:ring-white/10 hover:dark:bg-zinc-800/90"
-              >
-                <X className="h-[1.5rem] w-[1.5rem]" />
-                <span className="sr-only">Close</span>
-              </Button>
-            </SheetPrimitive.Close>
-          </div>
-          <div>
-            <h1 className="mt-4 text-2xl font-semibold">Suneet Misra</h1>
-          </div>
-          <div className="mt-2">{children}</div>
-        </SheetPrimitive.Content>
-      </SheetPortal>
-    ),
-    [side, className, children, mobileAvatar, ref, props]
+  return (
+    <SheetPortal>
+      <SheetOverlay />
+      <SheetPrimitive.Content
+        ref={ref}
+        className={cn(sheetVariants({ side }), className)}
+        {...props}
+      >
+        <div className="flex flex-row items-center justify-between">
+          <SheetPrimitive.Close asChild>
+            <Link href="/">{mobileAvatar}</Link>
+          </SheetPrimitive.Close>
+          <SheetPrimitive.Close asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="bg-white/90 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur hover:bg-white/90 hover:text-accent-foreground aria-expanded:text-accent-foreground md:hidden dark:bg-zinc-800/90 dark:ring-white/10 hover:dark:bg-zinc-800/90"
+            >
+              <X className="h-[1.5rem] w-[1.5rem]" />
+              <span className="sr-only">Close</span>
+            </Button>
+          </SheetPrimitive.Close>
+        </div>
+        <div>
+          <h1 className="mt-4 text-2xl font-semibold">Suneet Misra</h1>
+        </div>
+        <div className="mt-2">{children}</div>
+      </SheetPrimitive.Content>
+    </SheetPortal>
   );
-
-  return memoizedSheetPortal;
 });
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
-const SheetHeader = React.memo(
-  ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-    <div
-      className={cn(
-        "flex flex-col space-y-2 text-center sm:text-left",
-        className
-      )}
-      {...props}
-    />
-  )
+const SheetHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-2 text-center sm:text-left",
+      className
+    )}
+    {...props}
+  />
 );
-SheetHeader.displayName = "SheetHeader";
 
 const SheetFooter = ({
   className,
@@ -139,34 +127,31 @@ const SheetFooter = ({
     {...props}
   />
 );
-SheetFooter.displayName = "SheetFooter";
 
-const SheetTitle = React.memo(
-  React.forwardRef<
-    React.ElementRef<typeof SheetPrimitive.Title>,
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
-  >(({ className, ...props }, ref) => (
-    <SheetPrimitive.Title
-      ref={ref}
-      className={cn("text-lg font-semibold text-foreground", className)}
-      {...props}
-    />
-  ))
-);
+const SheetTitle = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cn("text-lg font-semibold text-foreground", className)}
+    {...props}
+  />
+));
+
 SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
-const SheetDescription = React.memo(
-  React.forwardRef<
-    React.ElementRef<typeof SheetPrimitive.Description>,
-    React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
-  >(({ className, ...props }, ref) => (
-    <SheetPrimitive.Description
-      ref={ref}
-      className={cn("text-sm text-muted-foreground", className)}
-      {...props}
-    />
-  ))
-);
+const SheetDescription = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Description>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Description
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+
 SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
 export {
