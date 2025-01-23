2e9f5f460860a57dbe8aab0ab191ffb666274faf
Suneet Misra <hello@suneet.codes>
2025-01-23 07:34:38 +0000

    🎨 Adds linkedin profile link to cards

--- components/testimonial-card.tsx
+++ components/testimonial-card.tsx
@@ -1,5 +1,6 @@
 import { SuspendedTestimonialCardAvatar } from "./testimonial-card-avatar";
 
+import Link from "next/link";
 import { memo } from "react";
 
 import { type Testimonial } from "@/types";
@@ -11,27 +12,33 @@ export const TestimonialCard = memo(
   async ({ testimonial }: TestimonialCardProps) => {
     return (
       <div className="pt-8 sm:inline-block sm:w-full sm:px-4">
-        <figure
-          className="rounded-2xl border-none bg-zinc-50 p-8 text-sm/6 shadow-none dark:bg-zinc-800/50"
-          role="article"
-          aria-labelledby={`testimonial-${testimonial.author.handle}`}
+        <Link
+          href={testimonial.author.profileUrl}
+          target="_blank"
+          rel="noopener norefferer"
         >
-          <blockquote>
-            <p className="italic">{`“${testimonial.shortBody}”`}</p>
-          </blockquote>
-          <figcaption className="mt-6 flex items-center gap-x-4">
-            <SuspendedTestimonialCardAvatar author={testimonial.author} />
-            <div>
-              <div className="font-semibold">{testimonial.author.name}</div>
-              <div
-                className="text-xs text-muted-foreground"
-                id={`testimonial-${testimonial.author.handle}`}
-              >
-                {testimonial.author.role}
+          <figure
+            className="rounded-2xl border-none bg-zinc-50 p-8 text-sm/6 shadow-none dark:bg-zinc-800/50"
+            role="article"
+            aria-labelledby={`testimonial-${testimonial.author.handle}`}
+          >
+            <blockquote>
+              <p className="italic">{`“${testimonial.shortBody}”`}</p>
+            </blockquote>
+            <figcaption className="mt-6 flex items-center gap-x-4">
+              <SuspendedTestimonialCardAvatar author={testimonial.author} />
+              <div>
+                <div className="font-semibold">{testimonial.author.name}</div>
+                <div
+                  className="text-xs text-muted-foreground"
+                  id={`testimonial-${testimonial.author.handle}`}
+                >
+                  {testimonial.author.role}
+                </div>
               </div>
-            </div>
-          </figcaption>
-        </figure>
+            </figcaption>
+          </figure>
+        </Link>
       </div>
     );
   }
