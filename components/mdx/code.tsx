26cdbb50427aeace2dc57d1b89cc1794f9d9e6a3
Suneet Misra <hello@suneet.codes>
2025-01-28 20:49:46 +0000

    🎨 Adds code-with-tabs component and updates article

--- components/mdx/code.tsx
+++ components/mdx/code.tsx
@@ -7,14 +7,14 @@ import { Pre, RawCode, highlight } from "codehike/code";
 export async function Code({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "dracula");
   const preClasses = clsx(
-    "m-0 rounded-b-xl",
+    "m-0 rounded-b-xl bg-zinc-950",
     highlighted.meta ? "rounded-t-none" : "rounded-t-xl"
   );
   return (
     <div className="relative my-8" role="region" aria-label="Code block">
-      <div className="rounded-t-xl bg-[var(--tw-prose-pre-bg)] px-4">
+      <div className="rounded-t-xl bg-muted px-4">
         {highlighted.meta && (
-          <div className="py-2 text-center text-sm text-zinc-400">
+          <div className="py-2 text-center text-sm text-zinc-950 dark:text-zinc-50">
             {highlighted.meta}
           </div>
         )}
