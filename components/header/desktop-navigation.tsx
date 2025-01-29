d96bf57f3cdec907a530c4a00762866e366f6679
Suneet Misra <hello@suneet.codes>
2025-01-29 14:44:56 +0000

    🎨 Updates theme-toggle to button

--- components/header/desktop-navigation.tsx
+++ components/header/desktop-navigation.tsx
@@ -14,7 +14,7 @@ export const DesktopNavigation = memo(
   ({ routeNames, ...rest }: DesktopNavigationProps) => {
     return (
       <nav {...rest}>
-        <ul className="flex rounded-md bg-white/90 px-3 text-sm font-medium shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
+        <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium shadow-lg ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10">
           {routeNames.map((title) => {
             const item = ROUTES.get(title);
             if (!item) {
