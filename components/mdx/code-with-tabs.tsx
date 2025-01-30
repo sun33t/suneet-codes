776959605fe21e89c198d2f051a8545a12e5135a
Suneet Misra <hello@suneet.codes>
2025-01-30 12:55:11 +0000

    🎨 Adds wordwrap to code-with-tabs

--- components/mdx/code-with-tabs.tsx
+++ components/mdx/code-with-tabs.tsx
@@ -1,44 +1,25 @@
-import { CopyButton } from "./copy-button";
+import { CodeTabs } from "./code-tabs";
 
 import { Block, CodeBlock, parseProps } from "codehike/blocks";
-import { Pre, type RawCode, highlight } from "codehike/code";
+import { type RawCode, highlight } from "codehike/code";
 import { z } from "zod";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs } from "@/components/ui/tabs";
 
 const Schema = Block.extend({ tabs: z.array(CodeBlock) });
 export async function CodeWithTabs(props: unknown) {
   const { tabs } = parseProps(props, Schema);
-  return <CodeTabs tabs={tabs} />;
+  return <CodeTabsContainer tabs={tabs} />;
 }
 
-export async function CodeTabs(props: { tabs: RawCode[] }) {
+async function CodeTabsContainer(props: { tabs: RawCode[] }) {
   const { tabs } = props;
   const highlighted = await Promise.all(
     tabs.map((tab) => highlight(tab, "dracula"))
   );
   return (
     <Tabs defaultValue={tabs[0]?.meta} className="relative my-8">
-      <TabsList>
-        {tabs.map((tab, index) => (
-          <TabsTrigger
-            key={tab.meta}
-            value={tab.meta}
-            className={index === 0 ? "rounded-tl-xl" : undefined}
-          >
-            {tab.meta}
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      {tabs.map((tab, i) => (
-        <TabsContent key={tab.meta} value={tab.meta} className="mt-0">
-          <CopyButton text={highlighted[i].code} />
-          <Pre
-            code={highlighted[i]}
-            className="m-0 rounded-b-xl rounded-t-none bg-zinc-950"
-          />
-        </TabsContent>
-      ))}
+      <CodeTabs highlighted={highlighted} tabs={tabs} />
     </Tabs>
   );
 }
